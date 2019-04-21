29b28e38e174e737c8deb430d053bc0677e86756
1459177541 <1459177541@qq.com>
2019-04-21 21:46:04 +0800

    [update]更新小型迷宫的路径生成逻辑，格式化代码风格

--- app.c
+++ app.c
@@ -20,8 +20,7 @@ typedef struct maze{
 /**
  * 数据类型
  * */
-typedef union data
-{
+typedef union data {
     void * Object;
     int Integer;
     double Double;
@@ -30,7 +29,7 @@ typedef union data
 /**
  * 节点
  * */
-typedef struct node{
+typedef struct node {
     pData d;
     struct node * last;
     struct node * next;
@@ -38,8 +37,7 @@ typedef struct node{
 /**
  * 链表
  * */
-typedef struct list
-{
+typedef struct list {
     pNode head;
     int (*add)(struct list *, pData, unsigned int);
     pData (*remove)(struct list *,unsigned int);
@@ -52,7 +50,7 @@ typedef struct list
 /**
  * 队列
  * */
-typedef struct queue{
+typedef struct queue {
     pList list;
     pNode tail;
     int (*offer)(struct queue *, pData);
@@ -63,14 +61,12 @@ typedef struct queue{
 /**
  * 堆栈
  * */
-typedef struct stack
-{
+typedef struct stack {
     pList list;
     pData (*pop)(struct stack*);
     void (*push)(struct stack *,pData);
     void (*free)(struct stack *);
 }stack, *pStack;
-
 
 #define EAST  1
 #define SOUTH  (1<<1)
@@ -80,6 +76,7 @@ typedef struct stack
 #define ROAD  0
 #define START  17
 #define END  18
+#define FLAG (1<<7)
 
 char* show[] = {
     "  ", "→ ", "↓ ", "↘ ",
@@ -95,7 +92,7 @@ char* show[] = {
  * index:索引，从0开始
  * return：是否失败
  * */
-int _list_add(pList self, pData data, unsigned int index){
+int _list_add(pList self, pData data, unsigned int index) {
     pNode node = (pNode)malloc(sizeof(node));
     node->d = data;
     if (0 == index) {
@@ -105,11 +102,11 @@ int _list_add(pList self, pData data, unsigned int index){
         return 0;
     }
     pNode p = self->head;
-    if(NULL == p){
+    if(NULL == p) {
         return 1;
     }
     int i = 0;
-    while(NULL != p->next){
+    while(NULL != p->next) {
         if (index == (++i)) {
             break;
         }
@@ -132,7 +129,7 @@ int _list_add(pList self, pData data, unsigned int index){
  * index:索引，从0开始
  * return:删除的值，为NULL表示失败;
  * */
-pData _list_remove(pList self, unsigned int index){
+pData _list_remove(pList self, unsigned int index) {
     if (NULL == self || NULL == self->head) {
         return NULL;
     }
@@ -150,7 +147,7 @@ pData _list_remove(pList self, unsigned int index){
     }
     pNode p = self->head->next;
     int i = 0;
-    while(NULL != p){
+    while(NULL != p) {
         if (index == (++i)) {
             pData d = p->d;
             if (NULL != p->next) {
@@ -172,11 +169,11 @@ pData _list_remove(pList self, unsigned int index){
  * index:索引，从0开始
  * return:该索引的值，为NULL表示越界
  * */
-pData _list_get(pList list, unsigned int index){
+pData _list_get(pList list, unsigned int index) {
     int i = 0;
     pNode p = list->head;
-    while(NULL != p){
-        if(index == (i++)){
+    while(NULL != p) {
+        if(index == (i++)) {
             return p->d;
         }
         p=p->next;
@@ -188,10 +185,10 @@ pData _list_get(pList list, unsigned int index){
  * 获取链表节点的数量
  * return:节点的数量
  * */
-unsigned int _list_size(pList self){
+unsigned int _list_size(pList self) {
     unsigned int size = 0;
     pNode p = self->head;
-    while(NULL != p){
+    while(NULL != p) {
         size++;
         p = p->next;
     }
@@ -202,9 +199,9 @@ unsigned int _list_size(pList self){
  * 遍历链表
  * consumer:消费者函数，即遍历链表每个元素做的事
  * */
-void _list_foreach(pList self, void (*consumer)(pData)){
+void _list_foreach(pList self, void (*consumer)(pData)) {
     pNode n = self->head;
-    while(NULL != n){
+    while(NULL != n) {
         consumer(n->d);
         n = n->next;
     }
@@ -214,14 +211,14 @@ void _list_foreach(pList self, void (*consumer)(pData)){
  * 链表是否为空
  * return:为空返回1,不为空返回0
  * */
-int _list_isNull(pList self){
+int _list_isNull(pList self) {
     return self->head == NULL;
 }
 
 /**
  * 删除链表
  * */
-void _list_free(pList self){
+void _list_free(pList self) {
     while(NULL != self->remove(self, 0));
     free(self);
 }
@@ -230,7 +227,7 @@ void _list_free(pList self){
  * 初始化链表
  * return:初始化的链表
  * */
-pList initList(){
+pList initList() {
     pList l = (pList)malloc(sizeof(list));
     l->head = NULL;
     l->add = _list_add;
@@ -247,7 +244,7 @@ pList initList(){
  * 入队
  * d:入队的数据
  * */
-int _queue_offer(pQueue self, pData d){
+int _queue_offer(pQueue self, pData d) {
     pNode n = (pNode)malloc(sizeof(node));
     n->d = d;
     n->next = NULL;
@@ -266,7 +263,7 @@ int _queue_offer(pQueue self, pData d){
  * 出队
  * return:出队的数据
  * */
-pData _queue_peek(pQueue self){
+pData _queue_peek(pQueue self) {
     if (1 == self->list->size(self->list)) {
         self->tail = NULL;
     }
@@ -277,11 +274,14 @@ pData _queue_peek(pQueue self){
  * 队列是否为空
  * return:为空返回1,不为空返回0
  * */
-int _queue_isNull(pQueue self){
+int _queue_isNull(pQueue self) {
     return self->list->isNull(self->list);
 }
 
-void _queue_free(pQueue self){
+/**
+ * 删除队列
+ * */
+void _queue_free(pQueue self) {
     self->list->free(self->list);
     free(self);
 }
@@ -290,7 +290,7 @@ void _queue_free(pQueue self){
  * 初始化队列
  * return:初始化的队列
  * */
-pQueue initQueue(){
+pQueue initQueue() {
     pQueue q = (pQueue)malloc(sizeof(queue));
     q->list = initList();
     q->tail = NULL;
@@ -305,7 +305,7 @@ pQueue initQueue(){
  * 压栈
  * data:数据
  * */
-void _pushStack(pStack self, pData data){
+void _stack_push(pStack self, pData data) {
     self->list->add(self->list, data, 0);
 }
 
@@ -313,11 +313,11 @@ void _pushStack(pStack self, pData data){
  * 弹栈
  * return:弹出的数据
  * */
-pData _popStack(pStack self){
+pData _stack_pop(pStack self) {
     pNode n = self->list->head;
     if (NULL == n) {
         return NULL;
-    }    
+    }
     self->list->head = n->next;
     if (NULL != self->list->head) {
         self->list->head->last = NULL;
@@ -333,7 +333,7 @@ pData _popStack(pStack self){
 /**
  * 删除堆栈
  * */
-void _freeStack(pStack self){
+void _stack_free(pStack self) {
     self->list->free(self->list);
     free(self);
 }
@@ -341,12 +341,12 @@ void _freeStack(pStack self){
 /**
  * 初始化堆栈
  * */
-pStack initStack(){
+pStack initStack() {
     pStack s = (pStack)malloc(sizeof(stack));
     s->list = initList();
-    s->pop = _popStack;
-    s->push = _pushStack;
-    s->free = _freeStack;
+    s->pop = _stack_pop;
+    s->push = _stack_push;
+    s->free = _stack_free;
     return s;
 }
 
@@ -356,38 +356,26 @@ pStack initStack(){
  * column:列
  * return:位置代码
  * */
-int _getPoint(pMaze self, int row, int column){
+int __maze_getPoint(pMaze self, int row, int column) {
     return (row%self->row)*self->column+(column%self->column);
-}
-
-/**
- * 获取位置信息
- * row:行
- * column:列
- * return:该位置的代码，参考全局变量show
- * */
-int _getByXY(pMaze self, int row, int column){
-    return self->maze[_getPoint(self, row, column)];
 }
 
 /**
  * 输出
  * */
-void _print(pMaze self){
-    for(int i = 0; i < self->row; i++)
-    {
-        for(int j = 0; j < self->column; j++)
-        {
-            printf(show[_getByXY(self, i ,j)&0x1F]);
+void _maze_show(pMaze self) {
+    for(int i = 0; i < self->row; i++) {
+        for(int j = 0; j < self->column; j++) {
+            printf(show[self->maze[__maze_getPoint(self, i, j)]&0x1F]);
         }
         printf("\n");
-    }    
+    }
 }
 
 /**
  * 删除迷宫
  * */
-void _freeMaze(pMaze self){
+void _maze_free(pMaze self) {
     free(self->maze);
     free(self);
 }
@@ -396,26 +384,28 @@ void _freeMaze(pMaze self){
  * 移动
  * q:队列
  * point:位置
+ * op:原位置
  * direction:方向
  * opposite:反方向
  * condition:条件
  * */
-void _moveMaze(pMaze self, pQueue q, 
+void __maze_move(pMaze self, pQueue q, 
                                 int point,int op,
                                 int direction,int opposite, 
-                                int condition){
+                                int condition) {
     if (condition                                                                                                               //初始条件
         && (0 == (self->maze[op] & opposite) || END == self->maze[op])      //不走原方向
         && 0 == (self->maze[point]&direction)                                                     //该方向没走过
-        // && 0 == (self->maze[point]&opposite)                                                         //新位置不到两条路
         && WALK != self->maze[point]                                                                          //不为墙
         && START != self->maze[point]                                                                         //不为开始
         && END != self->maze[point]) {                                                                        //不为结束
+            if (ROAD == self->maze[point]) {
+                pData d = (pData)malloc(sizeof(Data));
+                d->Integer = point;
+                q->offer(q, d);    
+            }
             self->maze[point] |= direction;
-            self->maze[point] |= 1<<6;
-            pData d = (pData)malloc(sizeof(Data));
-            d->Integer = point;
-            q->offer(q, d);    
+            self->maze[point] |= FLAG;
     }
 }
 
@@ -425,11 +415,13 @@ void _moveMaze(pMaze self, pQueue q,
  * direction:方向
  * p:当前位置
  * np:方向位置
+ * condition:条件
  * */
-void _removeFlag(pMaze self, pQueue q, int direction, int p, int np){
-    if (0 != (self->maze[np] & 1<<6) 
+void __maze_removeFlag(pMaze self, pQueue q, int direction, int p, int np, int condition) {
+    if (condition
+        && 0 != (self->maze[np] & FLAG) 
         && (direction == (self->maze[p]&direction) || START == self->maze[p])) {
-            self->maze[np] &= ~(1<<6);
+            self->maze[np] &= ~(FLAG);
             pData d = (pData)malloc(sizeof(Data));
             d->Integer = np;
             q->offer(q, d);
@@ -439,108 +431,112 @@ void _removeFlag(pMaze self, pQueue q, int direction, int p, int np){
 /**
  * 移动
  * */
-void _runMaze(pMaze self){
+void _maze_run(pMaze self) {
     pQueue q = initQueue();
     pData d = (pData)malloc(sizeof(Data));
     d->Integer = self->end;
     q->offer(q, d);
-    while(!q->isNull(q)){
+    while(!q->isNull(q)) {
         d = q->peek(q);
         int point = d->Integer;
         free(d);
         int r = point / self->column;
         int c = point % self->column;
-        _moveMaze(self, q, _getPoint(self, r+1, c), point, NORTH, SOUTH, r<(self->row-1));
-        _moveMaze(self, q, _getPoint(self, r, c-1), point, EAST, WEST, c>0);
-        _moveMaze(self, q, _getPoint(self, r-1, c), point, SOUTH, NORTH, r>0);
-        _moveMaze(self, q, _getPoint(self, r, c+1), point, WEST, EAST, c<(self->column-1));
+        __maze_move(self, q, __maze_getPoint(self, r+1, c), point, NORTH, SOUTH, r<(self->row-1));
+        __maze_move(self, q, __maze_getPoint(self, r, c-1), point, EAST, WEST, c>0);
+        __maze_move(self, q, __maze_getPoint(self, r-1, c), point, SOUTH, NORTH, r>0);
+        __maze_move(self, q, __maze_getPoint(self, r, c+1), point, WEST, EAST, c<(self->column-1));
     }
     d = (pData)malloc(sizeof(Data));
     d->Integer = self->start;
     q->offer(q, d);
-    while(!q->isNull(q)){
+    while(!q->isNull(q)) {
         d = q->peek(q);
         int point = d->Integer;
         free(d);
         int r = point / self->column;
         int c = point % self->column;
-        _removeFlag(self, q, NORTH, point, _getPoint(self, r-1, c));
-        _removeFlag(self, q, WEST, point, _getPoint(self, r, c-1));
-        _removeFlag(self, q, SOUTH, point, _getPoint(self, r+1, c));
-        _removeFlag(self, q, EAST, point, _getPoint(self, r, c+1));
+        __maze_removeFlag(self, q, NORTH, point, __maze_getPoint(self, r-1, c), r>0);
+        __maze_removeFlag(self, q, WEST, point, __maze_getPoint(self, r, c-1), c>0);
+        __maze_removeFlag(self, q, SOUTH, point, __maze_getPoint(self, r+1, c), r<(self->row-1));
+        __maze_removeFlag(self, q, EAST, point, __maze_getPoint(self, r, c+1), c<(self->column-1));
     }
     q->free(q);
-    for(int i = 0; i < self->row; i++)
-    {
-        for(int j = 0; j < self->column; j++)
-        {
-            int p = _getPoint(self, i, j);
-            if (0 != (self->maze[p] & 1<<6)) {
+    for(int i = 0; i < self->row; i++) {
+        for(int j = 0; j < self->column; j++) {
+            int p = __maze_getPoint(self, i, j);
+            if (0 != (self->maze[p] & FLAG)) {
                 self->maze[p] = ROAD;
             }
         }
-    }    
+    }
 }
 
 /**
  * 生成路径
  * seed:种子
  * */
-void _generateRoad(pMaze self, unsigned int seed){
+void _maze_generateRoad(pMaze self, unsigned int seed) {
     srand(seed);
+    int size = self->row * self->column ;
+    if (0 == self->row>>4 || 0 == self->column>>4) {
+        for(int i = (size>>1)-(size>>2); i < size; i++) {
+            self->maze[rand()%size] = ROAD;
+        }
+        do {
+            self->start = rand()%size;
+        } while (WALK != self->maze[self->start]);
+        self->maze[self->start] = START;
+        do {
+            self->end = rand()%size;
+        } while (WALK != self->maze[self->end]);
+        self->maze[self->end] = END;
+        return;
+    }
     int roadSize = ((self->row+self->column)<<2);
-    int point = rand() % (self->row * self->column);
+    int point = rand() % size;
     self->start = point;
     int generateRoadSize = 0;
     int r, c, l;
-    while(generateRoadSize < roadSize){
+    while(generateRoadSize < roadSize) {
         r = point / self->column;
         c = point % self->column;
         int direction = 1<<(rand()&2);
-        if (NORTH == direction || SOUTH == direction){
-            l = (self->row >> 3 > 0) 
-                        ? rand()%(self->row>>3) 
-                        : rand()%(self->row>>1);
-        }else {
-            l = (self->column >> 3 > 0) 
-                        ? rand()%(self->column>>3) 
-                        : rand()%(self->column>>1);
+        if (NORTH == direction || SOUTH == direction) {
+            l = rand()%(self->row>>3) ;
+        } else {
+            l = rand()%(self->column>>3);
         }
         for(int i = 0; i < l; i++) {
-            while(WALK != self->maze[point]){
+            while(WALK != self->maze[point]) {
                 if (rand()&1) {
                     break;
                 }
-                point = rand()%(self->row * self->column);
+                point = rand()%size;
                 r = point / self->column;
                 c = point % self->column;
                 direction = 1<<(rand()&3);
             }
             self->maze[point] = ROAD;
-            switch (direction) {
-                case NORTH:
-                    r--;
-                    break;
-                case SOUTH:
-                    r++;
-                    break;
-                case EAST:
-                    c--;
-                    break;
-                case WEST:
-                    c++;
-                    break;
+            if ( NORTH == direction) {
+                r--;
+            } else if (SOUTH == direction) {
+                r++;
+            } else if (EAST == direction) {
+                c--;
+            } else if (WEST == direction) {
+                c++;
             }
-            point = _getPoint(self, r, c);
+            point = __maze_getPoint(self, r, c);
         }
         if (rand()&3) {
-            point = rand()%(self->row * self->column);
+            point = rand()%size;
         } else {
-            point = _getPoint(self, r, c);
+            point = __maze_getPoint(self, r, c);
         }
         generateRoadSize++;
     }
-    self->end = _getPoint(self, r, c);
+    self->end = __maze_getPoint(self, r, c);
     self->maze[self->end] = END;
     self->maze[self->start] = START;
 }
@@ -549,12 +545,11 @@ void _generateRoad(pMaze self, unsigned int seed){
  * 保存到文件
  * fileName:要保存的文件名
  * */
-void _saveToFile(pMaze self, char* fileName){
+void _maze_save(pMaze self, char* fileName) {
     FILE *file = fopen(fileName, "w");
     fprintf(file, "%d %d\n", self->row, self->column);
     int size = self->row * self->column;
-    for(int i = 0; i < size; i++)
-    {
+    for(int i = 0; i < size; i++) {
         fprintf(file, "%d ", self->maze[i]);
     }
     fclose(file);
@@ -565,28 +560,27 @@ void _saveToFile(pMaze self, char* fileName){
  * fileName:文件名
  * return:初始化的迷宫
  * */
-pMaze initMazeByFile(char *fileName){
+pMaze initMazeByFile(char *fileName) {
     FILE *file = fopen(fileName,"r");
     pMaze ret = (pMaze)malloc(sizeof(maze));
     fscanf(file, "%d %d\n",&(ret->row), &(ret->column));
     int size = ret->row*ret->column;
     int *m = (int*)malloc(sizeof(int)*size);
-    for(int i = 0; i < size; i++)
-    {
+    for(int i = 0; i < size; i++) {
         fscanf(file, "%d", &(m[i]));
         if (START == m[i]) {
             ret->start = i;
-        }else if (END == m[i]) {
+        } else if (END == m[i]) {
             ret->end = i;
         }
     }
     fclose(file);
     ret->maze = m;
-    ret->print = _print;
-    ret->generate = _generateRoad;
-    ret->save = _saveToFile;
-    ret->free = _freeMaze;
-    ret->run = _runMaze;
+    ret->print = _maze_show;
+    ret->generate = _maze_generateRoad;
+    ret->save = _maze_save;
+    ret->free = _maze_free;
+    ret->run = _maze_run;
     return ret;
 }
 
@@ -596,31 +590,30 @@ pMaze initMazeByFile(char *fileName){
  * column:列数
  * return:初始化的迷宫
  * */
-pMaze initMaze(unsigned int row, unsigned int column){
+pMaze initMaze(unsigned int row, unsigned int column) {
     pMaze ret = (pMaze)malloc(sizeof(maze));
     ret->row = row;
     ret->column = column;
     int size = row*column;
     int* m = (int*)malloc(sizeof(int)*size);
-    for(int i = 0; i < size; i++)
-    {
+    for(int i = 0; i < size; i++) {
         m[i]=WALK;
     }
     ret->start = -1;
     ret->end = -1;
     ret->maze = m;
-    ret->print = _print;
-    ret->generate = _generateRoad;
-    ret->save = _saveToFile;
-    ret->free = _freeMaze;
-    ret->run = _runMaze;
+    ret->print = _maze_show;
+    ret->generate = _maze_generateRoad;
+    ret->save = _maze_save;
+    ret->free = _maze_free;
+    ret->run = _maze_run;
     return ret;
 }
 
 /**
  * 主方法
  * */
-int main(){
+int main() {
     pMaze maze = initMaze(20, 40);
     maze->generate(maze, time(NULL));
     maze->print(maze);
